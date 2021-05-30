16a6c8a387d3dd519a176e93d2c7e6232e7e09f8
Samuel Howard <samuelhowardstudent@gmail.com>
2021-05-30 19:27:17 -0400

    Improves searching

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,6 +1,7 @@
 from flask_app import app, db
 from flask import render_template, request, redirect, url_for
 from flask_app.models import *
+from sqlalchemy import or_
 
 @app.route('/')
 @app.route('/index')
@@ -23,7 +24,12 @@ def projects_search():
     page = request.args.get('page', 1, type=int)
     if request.method == 'POST':
         search_term=request.form['search_term']
-        search_results_data = Projects.query.filter(Projects.description.contains(search_term))
+        search_results_data = Projects.query.filter(
+            or_(Projects.description.contains(search_term),
+                Projects.name.contains(search_term),
+                Projects.language.contains(search_term),
+                Projects.owner.contains(search_term),
+                Projects.source.contains(search_term)))
         return redirect(request.path)
     else:
         return render_template('projects_search.html', search_results=search_results_data.paginate(page=page, per_page=50), search_term=search_term)
