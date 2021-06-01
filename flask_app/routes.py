0161d83ace158ddf1b081271836eb9fe146d437f
Samuel Howard <samuelhowardstudent@gmail.com>
2021-06-01 12:17:05 -0400

    Adds search bar to organizations

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -12,16 +12,20 @@ def index():
     return render_template('projects.html', projects=Projects.query.paginate(page=page, per_page=50))
 '''
 
+orgs_search_results_data = Organizations.query.filter(Organizations.name.contains(""))
+projects_search_results_data = Projects.query.filter(Projects.description.contains(""))
+search_term = None
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 @app.route('/projects', methods=['GET', 'POST'])
 def projects():
-    global search_results_data
+    global projects_search_results_data
     global search_term
     page = request.args.get('page', 1, type=int)
     if request.method == 'POST':
         search_term=request.form['search_term']
-        search_results_data = Projects.query.filter(
+        projects_search_results_data = Projects.query.filter(
             or_(Projects.description.contains(search_term),
                 Projects.name.contains(search_term),
                 Projects.language.contains(search_term),
@@ -29,15 +33,20 @@ def projects():
                 Projects.source.contains(search_term)))
         return redirect(request.path)
     else:
-        return render_template('projects_search.html', search_results=search_results_data.paginate(page=page, per_page=50), search_term=search_term)
+        return render_template('projects_search.html', search_results=projects_search_results_data.paginate(page=page, per_page=50), search_term=search_term)
 
-@app.route('/organizations')
+@app.route('/organizations', methods=['GET', 'POST'])
 def organizations():
+    global orgs_search_results_data
+    global search_term
     page = request.args.get('page', 1, type=int)
-    return render_template('organizations.html', organizations=Organizations.query.paginate(page=page, per_page=50), projects=Projects.query.all())
+    if request.method == 'POST':
+        search_term=request.form['search_term']
+        orgs_search_results_data = Organizations.query.filter(Organizations.name.contains(search_term))
+        return redirect(request.path)
+    else:
+        return render_template('organizations.html', search_results=orgs_search_results_data.paginate(page=page, per_page=50), search_term=search_term, projects=Projects.query.all())
 
-search_results_data = Projects.query.filter(Projects.description.contains(""))
-search_term = None
 
 '''
 @app.route('/', methods=['GET', 'POST'])
