747f645a1aa63e86e3613144537f5b2a77307fc7
Samuel Howard <samuelhowardstudent@gmail.com>
2021-06-07 12:56:10 -0400

    Adds individual project pages

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -3,15 +3,6 @@ from flask import render_template, request, redirect, url_for
 from flask_app.models import *
 from sqlalchemy import or_, func
 
-'''
-@app.route('/')
-@app.route('/index')
-@app.route('/projects')
-def index():
-    page = request.args.get('page', 1, type=int)
-    return render_template('projects.html', projects=Projects.query.paginate(page=page, per_page=50))
-'''
-
 orgs_search_results_data = Organizations.query.filter(Organizations.name.contains(""))
 projects_search_results_data = Projects.query.filter(Projects.description.contains(""))
 search_term = None
@@ -62,24 +53,8 @@ def organizations():
     else:
         return render_template('organizations.html', search_results=orgs_search_results_data.paginate(page=page, per_page=50), search_term=search_term, projects=Projects.query.all())
 
+@app.route('/project/<projectName>')
+def project(projectName):
+    proj = Projects.query.filter(Projects.name==projectName).one()
+    return render_template('project.html', project=proj)
 
-'''
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
-@app.route('/projects', methods=['GET', 'POST'])
-def projects_search():
-    global search_results_data
-    global search_term
-    page = request.args.get('page', 1, type=int)
-    if request.method == 'POST':
-        search_term=request.form['search_term']
-        search_results_data = Projects.query.filter(
-            or_(Projects.description.contains(search_term),
-                Projects.name.contains(search_term),
-                Projects.language.contains(search_term),
-                Projects.owner.contains(search_term),
-                Projects.source.contains(search_term)))
-        return redirect(request.path)
-    else:
-        return render_template('projects.html', search_results=search_results_data.paginate(page=page, per_page=50), search_term=search_term)
-'''
