c981c1bd0c0e66db1004113f7951fff0503a47f7
Samuel Howard <samuelhowardstudent@gmail.com>
2021-06-12 11:33:47 -0400

    Adds items per page search option

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -6,6 +6,8 @@ from sqlalchemy import or_, func
 orgs_search_results_data = Organizations.query.filter(Organizations.name.contains(""))
 projects_search_results_data = Projects.query.filter(Projects.description.contains(""))
 search_term = None
+projectsPerPage = 25
+orgsPerPage = 25
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -13,6 +15,7 @@ search_term = None
 def projects():
     global projects_search_results_data
     global search_term
+    global projectsPerPage
     page = request.args.get('page', 1, type=int)
     if request.method == 'POST':
         search_term=request.form['search_term']
@@ -22,6 +25,8 @@ def projects():
                 Projects.language.contains(search_term),
                 Projects.owner.contains(search_term),
                 Projects.source.contains(search_term)))
+        if 'per_page' in request.form:
+            projectsPerPage = int(request.form['per_page'])
         if 'sort_by' in request.form and request.form['sort_by'] == 'name':
             if 'reverse' in request.form:
                 projects_search_results_data = projects_search_results_data.order_by(Projects.name.desc())
@@ -34,16 +39,19 @@ def projects():
                 projects_search_results_data = projects_search_results_data.order_by(func.date(Projects.created_time).desc())
         return redirect(request.path)
     else:
-        return render_template('projects_search.html', search_results=projects_search_results_data.paginate(page=page, per_page=50), search_term=search_term)
+        return render_template('projects_search.html', search_results=projects_search_results_data.paginate(page=page, per_page=projectsPerPage), search_term=search_term)
 
 @app.route('/organizations', methods=['GET', 'POST'])
 def organizations():
     global orgs_search_results_data
     global search_term
+    global orgsPerPage
     page = request.args.get('page', 1, type=int)
     if request.method == 'POST':
         search_term=request.form['search_term']
         orgs_search_results_data = Organizations.query.filter(Organizations.name.contains(search_term))
+        if 'per_page' in request.form:
+                        orgsPerPage = int(request.form['per_page'])
         if 'name' in request.form:
             if 'reverse' in request.form:
                 orgs_search_results_data = orgs_search_results_data.order_by(Organizations.name.desc())
@@ -51,7 +59,7 @@ def organizations():
                 orgs_search_results_data = orgs_search_results_data.order_by(Organizations.name)
         return redirect(request.path)
     else:
-        return render_template('organizations.html', search_results=orgs_search_results_data.paginate(page=page, per_page=50), search_term=search_term, projects=Projects.query.all())
+        return render_template('organizations.html', search_results=orgs_search_results_data.paginate(page=page, per_page=orgsPerPage), search_term=search_term, projects=Projects.query.all())
 
 @app.route('/project/<projectName>')
 def project(projectName):
